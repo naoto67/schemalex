d0442be192e13f0ed0b22b129f8a2b94dcdb27ad
naoto67 <uver.com67@gmail.com>
2020-12-13 20:25:19 +0900

    init

---
+++ code/go.mod
@@ -0,0 +1,5 @@
+module schema
+
+go 1.15
+
+require github.com/schemalex/schemalex v0.1.1 // indirect
---
+++ code/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+
+	"github.com/schemalex/schemalex/diff"
+)
+
+func main() {
+	const sql1 = `CREATE TABLE hoge (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id)
+);`
+	const sql2 = `CREATE TABLE hoge (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    c VARCHAR (20) NOT NULL DEFAULT "hoge",
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE fuga (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id)
+);`
+
+	diff.Strings(os.Stdout, sql1, sql2, diff.WithTransaction(true))
+
+	// OUTPUT:
+	// BEGIN;
+	//
+	// SET FOREIGN_KEY_CHECKS = 0;
+	//
+	// CREATE TABLE `fuga` (
+	// `id` INTEGER NOT NULL AUTO_INCREMENT,
+	// PRIMARY KEY (`id`)
+	// );
+	//
+	// ALTER TABLE `hoge` ADD COLUMN `c` VARCHAR (20) NOT NULL DEFAULT "hoge";
+	//
+	// SET FOREIGN_KEY_CHECKS = 1;
+	//
+	// COMMIT;
+}
